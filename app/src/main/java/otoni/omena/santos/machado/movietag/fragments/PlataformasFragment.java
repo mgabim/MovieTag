e992a0a5b2d9e8e398c661487dd5e81589e950b6
movietag <xucagabi22@gmail.com>
2024-11-19 11:06:37 -0300

    Initial commit

---
+++ app/src/main/java/otoni/omena/santos/machado/movietag/fragments/PlataformasFragment.java
@@ -0,0 +1,66 @@
+package otoni.omena.santos.machado.movietag.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import otoni.omena.santos.machado.movietag.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link PlataformasFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class PlataformasFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public PlataformasFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment PlataformasFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static PlataformasFragment newInstance(String param1, String param2) {
+        PlataformasFragment fragment = new PlataformasFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_plataformas, container, false);
+    }
+}
