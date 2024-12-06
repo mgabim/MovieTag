2e1d01f043e88eaaeaf537de8801c5102d309b36
guihocosta <guiihocosta@gmail.com>
2024-12-06 09:11:54 -0300

    Adicionando os Adapters

--- app/src/main/java/otoni/omena/santos/machado/movietag/activities/MainActivity.java
+++ app/src/main/java/otoni/omena/santos/machado/movietag/activities/MainActivity.java
@@ -1,26 +1,65 @@
 package otoni.omena.santos.machado.movietag.activities;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 import otoni.omena.santos.machado.movietag.R;
+import otoni.omena.santos.machado.movietag.fragments.HomeFragment;
+import otoni.omena.santos.machado.movietag.fragments.MinhasListasFragment;
+import otoni.omena.santos.machado.movietag.fragments.PerfilFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Criar ViewModel
+
+        bottomNavigationView = findViewById(R.id.btNavPrincipal);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+                if (itemId == R.id.inicioViewOp){
+                    HomeFragment homeFragment = HomeFragment.newInstance();
+                    setFragment(homeFragment);
+                }
+                if (itemId == R.id.minhasListasViewOp){
+                    MinhasListasFragment minhasListasFragment = MinhasListasFragment.newInstance();
+                    setFragment(minhasListasFragment);
+                }
+
+                if (itemId == R.id.perfilUsuarioOp){
+                    // Testar se a pessoa está logada
+                    PerfilFragment perfilFragment = PerfilFragment.newInstance();
+                    setFragment(perfilFragment);
+                }
+                return true;
+
+            }
         });
+
     }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.flPrincipal, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+
 }
