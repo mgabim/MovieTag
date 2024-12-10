77938ef19c5c7dd0b7ddee9d001a3cbd88f10b76
lnoliver96 <luanotoni4@gmail.com>
2024-12-10 11:00:20 -0300

    Commit addProdListaadapter

--- app/src/main/java/otoni/omena/santos/machado/movietag/adapters/resultadosBuscaAdapter.java
+++ app/src/main/java/otoni/omena/santos/machado/movietag/adapters/resultadosBuscaAdapter.java
@@ -1,4 +1,18 @@
 package otoni.omena.santos.machado.movietag.adapters;
 
-public class resultadosBuscaAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+
+import otoni.omena.santos.machado.movietag.R;
+import otoni.omena.santos.machado.movietag.models.MyViewHolder;
+
+public class resultadosBuscaAdapter extends {
+
+
+
+
+
 }
---
+++ app/src/main/java/otoni/omena/santos/machado/movietag/models/MyViewHolder.java
@@ -0,0 +1,12 @@
+package otoni.omena.santos.machado.movietag.models;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyViewHolder extends RecyclerView.ViewHolder {
+    public MyViewHolder(@NonNull View itemView){
+        super(itemView);
+    }
+}
