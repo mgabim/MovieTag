932deb7bb78ca8142b2067a18c57f2567b5bc257
guihocosta <guiihocosta@gmail.com>
2024-12-11 12:50:40 -0300

    Login e Cadastro activity

--- app/src/main/java/otoni/omena/santos/machado/movietag/activities/CadastroActivity.java
+++ app/src/main/java/otoni/omena/santos/machado/movietag/activities/CadastroActivity.java
@@ -1,6 +1,10 @@
 package otoni.omena.santos.machado.movietag.activities;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,19 +12,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import otoni.omena.santos.machado.movietag.R;
 
 public class CadastroActivity extends AppCompatActivity {
-
+    private ImageView imgFotoPerfil;
+    private FloatingActionButton fbAddFotoPerfil;
+    private FloatingActionButton fbVoltarLogin;
+    private EditText etUsuario, etEmail, etSenha, etConfirmaSenha;
+    private Button btnLogar;
+    private TextView tvJaPossuiLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_cadastro);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Inicializar componentes
+        imgFotoPerfil = findViewById(R.id.imgFotoPerfil);
+        fbAddFotoPerfil = findViewById(R.id.fbAddFotoPerfil);
+        fbVoltarLogin = findViewById(R.id.fbVoltarLogin);
+        etUsuario = findViewById(R.id.etUsuario);
+        etEmail = findViewById(R.id.etEmail);
+        etSenha = findViewById(R.id.etSenha);
+        etConfirmaSenha = findViewById(R.id.etConfirmaSenha);
+        btnLogar = findViewById(R.id.btnLogar);
+        tvJaPossuiLogin = findViewById(R.id.tvJaPossuiLogin);
+
+
     }
 }
