c2a954bb3e9919178c18f46e42f14795b29bcfc3
guihocosta <guiihocosta@gmail.com>
2024-12-12 11:25:17 -0300

    Login e Cadastro

--- app/src/main/java/otoni/omena/santos/machado/movietag/activities/CadastroActivity.java
+++ app/src/main/java/otoni/omena/santos/machado/movietag/activities/CadastroActivity.java
@@ -1,10 +1,13 @@
 package otoni.omena.santos.machado.movietag.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,27 +20,51 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import otoni.omena.santos.machado.movietag.R;
 
 public class CadastroActivity extends AppCompatActivity {
-    private ImageView imgFotoPerfil;
-    private FloatingActionButton fbAddFotoPerfil;
-    private FloatingActionButton fbVoltarLogin;
-    private EditText etUsuario, etEmail, etSenha, etConfirmaSenha;
-    private Button btnLogar;
-    private TextView tvJaPossuiLogin;
+    EditText etUsuarioCadastro, etEmailCadastro, etConfirmarSenha, etSenhaCadastro;
+    TextView tvPossuiLogin;
+    Button btnCadastrar;
+    String email, usuario, senha, confirmaSenha;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
 
-        // Inicializar componentes
-        imgFotoPerfil = findViewById(R.id.imgFotoPerfil);
-        fbAddFotoPerfil = findViewById(R.id.fbAddFotoPerfil);
-        fbVoltarLogin = findViewById(R.id.fbVoltarLogin);
-        etUsuario = findViewById(R.id.etUsuario);
-        etEmail = findViewById(R.id.etEmail);
-        etSenha = findViewById(R.id.etSenha);
-        etConfirmaSenha = findViewById(R.id.etConfirmaSenha);
-        btnLogar = findViewById(R.id.btnLogar);
-        tvJaPossuiLogin = findViewById(R.id.tvJaPossuiLogin);
+
+        // Lógica para criar imagem ou adicionar imagem da Galeria
+
+        tvPossuiLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(CadastroActivity.this, LoginActivity.class);
+                startActivity();
+            }
+        });
+
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etEmailCadastro = findViewById(R.id.etEmailCadastro);
+                email = etEmailCadastro.getText().toString();
+
+                etUsuarioCadastro = findViewById(R.id.etUsuarioCadastro);
+                usuario = etUsuarioCadastro.getText().toString();
+
+                etSenhaCadastro = findViewById(R.id.etSenhaCadastro);
+                senha = etSenhaCadastro.getText().toString();
+
+                etConfirmarSenha = findViewById(R.id.etConfirmarSenhaCadastro);
+                confirmaSenha = etConfirmarSenha.getText().toString();
+
+                // Verificações
+
+                Intent i = new Intent(CadastroActivity.this, MainActivity.class);
+                startActivity(i);
+
+            }
+        });
+
+
 
 
     }
--- app/src/main/java/otoni/omena/santos/machado/movietag/activities/LoginActivity.java
+++ app/src/main/java/otoni/omena/santos/machado/movietag/activities/LoginActivity.java
@@ -1,6 +1,11 @@
 package otoni.omena.santos.machado.movietag.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,9 +16,37 @@ import androidx.core.view.WindowInsetsCompat;
 import otoni.omena.santos.machado.movietag.R;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText etUsuarioCadastro, etEmailCadastro, etConfirmarSenha, etSenhaCadastro;
+    TextView tvIrParaCadastro;
+    Button btnLogar;
+    String usuarioEmail, senha;
+}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Lógica para criar imagem ou adicionar imagem da Galeria
+
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etEmailCadastro = findViewById(R.id.etEmailCadastro);
+                email = etEmailCadastro.getText().toString();
+
+                etUsuarioCadastro = findViewById(R.id.etUsuarioCadastro);
+                usuario = etUsuarioCadastro.getText().toString();
+
+                etSenhaCadastro = findViewById(R.id.etSenhaCadastro);
+                senha = etSenhaCadastro.getText().toString();
+
+                etConfirmarSenha = findViewById(R.id.etConfirmarSenhaCadastro);
+                confirmaSenha = etConfirmarSenha.getText().toString();
+
+                // Verificações
+
+                Intent i = new Intent(CadastroActivity.this, MainActivity.class);
+                startActivity(i);
     }
 }
