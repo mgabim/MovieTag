e3dc26f5e29ac32a09222a768f6c1680bdce6dfc
lnoliver96 <luanotoni4@gmail.com>
2024-12-13 09:05:57 -0300

    Commit criando ResultadosBuscaAdapter

--- app/src/main/java/otoni/omena/santos/machado/movietag/adapters/ResultadosBuscaAdapter.java
+++ app/src/main/java/otoni/omena/santos/machado/movietag/adapters/ResultadosBuscaAdapter.java
@@ -1,26 +1,48 @@
 package otoni.omena.santos.machado.movietag.adapters;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
+import otoni.omena.santos.machado.movietag.R;
+import otoni.omena.santos.machado.movietag.fragments.ResultadoBuscaFragment;
+import otoni.omena.santos.machado.movietag.models.Lista;
+import otoni.omena.santos.machado.movietag.models.MyViewHolder;
+
 public class ResultadosBuscaAdapter extends RecyclerView.Adapter {
+    ResultadoBuscaFragment resultadoBuscaFragment;
+    List<Lista> listaResultadoBusca;
+
+    public ResultadosBuscaAdapter(ResultadoBuscaFragment resultadoBuscaFragment,List<Lista> listaResultadoBusca) {
+        this.resultadoBuscaFragment = resultadoBuscaFragment;
+        this.listaResultadoBusca = listaResultadoBusca;
+    }
 
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        // Inflador serve para ler arquivo xml do item e criar os elementos de interface
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+
+        // Recebe o constraint layout
+        View v = inflater.inflate(R.layout.fragment_resultado_busca, parent, false);
+        return new MyViewHolder(v);
+
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+        RecyclerView rvResultados = (RecyclerView)holder.itemView.findViewById(R.id.rvResultados);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listaResultadoBusca.size();
     }
 }
