bb9ae7154c727009f25a1b5890d9ee560b957e8c
lnoliver96 <luanotoni4@gmail.com>
2024-12-17 09:01:47 -0300

    Config IntegranteAdapter

--- app/src/main/java/otoni/omena/santos/machado/movietag/fragments/IntegranteFragment.java
+++ app/src/main/java/otoni/omena/santos/machado/movietag/fragments/IntegranteFragment.java
@@ -2,6 +2,8 @@ package otoni.omena.santos.machado.movietag.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,20 +11,35 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import otoni.omena.santos.machado.movietag.R;
+import otoni.omena.santos.machado.movietag.activities.MainActivity;
+import otoni.omena.santos.machado.movietag.models.Integrante;
 
 public class IntegranteFragment extends Fragment {
+    MainActivity mainActivity;
+    Integrante integrante;
 
-    public IntegranteFragment() {
+
+    public IntegranteFragment(Integrante integrante, MainActivity mainActivity) {
         // Required empty public constructor
+        this.integrante = integrante;
+        this.mainActivity = mainActivity;
     }
 
-    public static IntegranteFragment newInstance() {
-        return new IntegranteFragment();
+    public static IntegranteFragment newInstance(Integrante integrante, MainActivity mainActivity) {
+
+        return new IntegranteFragment(integrante, mainActivity);
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
     }
 
