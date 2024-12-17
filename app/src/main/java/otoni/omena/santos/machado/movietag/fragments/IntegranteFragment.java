7d4defc2cbc2ee6342e2b25b62c099108364da3b
Lucas Santos <161741045+LuzKS@users.noreply.github.com>
2024-12-17 11:13:15 -0300

    SDWEWFFFW

--- app/src/main/java/otoni/omena/santos/machado/movietag/fragments/IntegranteFragment.java
+++ app/src/main/java/otoni/omena/santos/machado/movietag/fragments/IntegranteFragment.java
@@ -4,11 +4,15 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -75,7 +79,35 @@ public class IntegranteFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_integrante, container, false);
+        // Inflate the fragment's layout
+        View rootView = inflater.inflate(R.layout.fragment_integrante, container, false);
+
+        // Configure the Toolbar
+        Toolbar toolbar = rootView.findViewById(R.id.tbIntegrante);
+
+        // setando como actiobar do fragment
+        if (getActivity() != null) {
+            AppCompatActivity activity = (AppCompatActivity) getActivity();
+            activity.setSupportActionBar(toolbar);
+
+            // colocando o titulo de acordo com nome de integrante
+            if (activity.getSupportActionBar() != null && integrante != null) {
+                activity.getSupportActionBar().setTitle(integrante.getNome());
+                activity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+                activity.getSupportActionBar().setHomeAsUpIndicator(android.R.drawable.ic_media_previous);
+            }
+
+            toolbar.setNavigationOnClickListener(v -> activity.onBackPressed());
+        }
+
+        setHasOptionsMenu(true);
+
+        return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater){
+        inflater.inflate(R.menu.menu_integrante, menu);
+        super.onCreateOptionsMenu(menu, inflater);
     }
 }
