d05ae8d564bad898d08719b3442f30668ddf8ab2
Guilherme Costa <guiihocosta@gmail.com>
2024-12-18 11:00:01 -0300

    Código do Omena

--- app/src/main/java/otoni/omena/santos/machado/movietag/fragments/TagFragment.java
+++ app/src/main/java/otoni/omena/santos/machado/movietag/fragments/TagFragment.java
@@ -2,22 +2,35 @@ package otoni.omena.santos.machado.movietag.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import otoni.omena.santos.machado.movietag.R;
+import otoni.omena.santos.machado.movietag.activities.MainActivity;
+import otoni.omena.santos.machado.movietag.adapters.FilmesAssociadosTagAdapter;
+import otoni.omena.santos.machado.movietag.models.Tag;
 
 public class TagFragment extends Fragment {
+    MainActivity mainActivity;
+    Tag tag;
 
-    public TagFragment() {
-        // Required empty public constructor
+    public TagFragment(MainActivity mainActivity, Tag tag) {
+        this.mainActivity = mainActivity;
+        this.tag = tag;
     }
 
-    public static TagFragment newInstance() {
-        return new TagFragment();
+    public static TagFragment newInstance(MainActivity mainActivity, Tag tag) {
+        return new TagFragment(mainActivity, tag);
     }
 
     @Override
@@ -30,5 +43,19 @@ public class TagFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tag, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Código para colocar tag na toolbar
+
+        TextView tvNumAcessos = view.findViewById(R.id.tvNumAcessos);
+        RecyclerView rvFilmesAssociadosTag = view.findViewById(R.id.rvFilmesAssociadosTag);
+
+        FilmesAssociadosTagAdapter filmesAdapter = new FilmesAssociadosTagAdapter(mainActivity, tag);
+        rvFilmesAssociadosTag.setAdapter(filmesAdapter);
+        rvFilmesAssociadosTag.setLayoutManager(new LinearLayoutManager(getContext()));
+
     }
 }
